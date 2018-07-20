672d4fecca64352531c7a63810f4a6311aa26ce0
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-20 17:03:01 +0000

    automatically backed up by learn

--- db/migrate/007_add_actorid_to_characters.rb
+++ db/migrate/007_add_actorid_to_characters.rb
@@ -2,5 +2,6 @@ class AddActoridToCharacters < ActiveRecord::Migration[4.2]
   def change
     add_column :characters, :actor_id, :integer
     add_column :characters, :catchphrase, :string
+    add_column :characters, :network_id, :integer
   end
 end
