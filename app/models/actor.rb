61b7da0c80f9b7d1e29d4539a3e3d4ebba889d3c
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-20 17:08:30 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,10 @@
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
+
+  def full_name
+    a = self.first_name
+    b = self.last_name
+    "#{a} #{b}"
+  end 
 end
