c6d5c20ea5a626e2f50cc863e4f30c490e825b87
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-20 17:11:34 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,8 +4,8 @@ class Character < ActiveRecord::Base
   belongs_to :network
 
   def say_that_thing_you_say
-    a = self.name 
+    a = self.name
     b = self.catchphrase
     "#{a} always says: #{b}"
-  end   
+  end
 end
